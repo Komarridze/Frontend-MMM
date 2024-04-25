c94005cc140c9e760b153a3ae3499ef75ed30d87
Komarridze <aychihuahua7@gmail.com>
2024-04-25 19:19:53 +0300

    1.1.6G+: Added messaging

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import sqlite3 from 'sqlite3'
 import {open} from 'sqlite'
+import fs from 'fs'
 
 const dbPromise = open({
     filename: "data.db",
@@ -14,6 +15,7 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser'
 import 'localstorage-polyfill'
+import { getLocal } from 'rxdb/plugins/local-documents'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -176,6 +178,44 @@ app.post('/main', urlencodedParser, async (req, res) => {
     
     
 })
+
+app.post('/openchat/:chatID', urlencodedParser, async (req, res) => {
+    var chatID = req.params.chatID;
+    var chatdata = JSON.parse(fs.readFileSync(path.join(__dirname + `/private/dm/${chatID}.json`)));
+    console.log(req.body.newmessage)
+    if (req.body.newmessage != '') {
+        chatdata.messages.push({
+            "sender": global.localStorage.getItem('userName'),
+            "time": "undefined",
+            "text": req.body.newmessage,
+            "reactions": "none",
+            "status": "received"
+        })
+
+        fs.writeFileSync(path.join(__dirname + `/private/dm/${chatID}.json`), JSON.stringify(chatdata));
+        chatdata = JSON.parse(fs.readFileSync(path.join(__dirname + `/private/dm/${chatID}.json`)));
+
+    }
+
+
+    console.log(chatdata.messages)
+    let msgs = '';
+    for (let message of chatdata.messages) {
+        msgs += `<msg class="inter"><i style="font-size:60%;color:lightgray;">${message.sender}: &nbsp;</i>${message.text}</msg>`;
+    }
+
+    let data = {
+        username: global.localStorage.getItem('userName'),
+        tag: global.localStorage.getItem('userKey'),
+        messages: msgs
+    }
+
+    res.render('main', {
+        userData: data
+
+    });
+    
+});
 
 const setup = async () => {
     const db = await dbPromise;
